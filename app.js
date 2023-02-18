fffc63edebc13379bd3c6e2eff1661de746a67f8
dev0869 <108985813+dev0869@users.noreply.github.com>
2023-02-18 05:04:08 -0500

    change

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const Connect=require('./database/Connect')
 const express=require('express');
 const Router = require('./Routes/Product');
+const cors=require('cors')
 const app=express();
 
 const PORT =5000;
 app.get('/',(req,res)=>{
     res.send('hii world')
 })
-
+app.use(cors({
+    origin:"*",
+    credentials:true
+}))
 app.use('/DeveshApi/Products/',Router)
 
 Connect().then(console.log('mongo is connected')).catch((error)=>{
