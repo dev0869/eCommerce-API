75df0d5888586c8cc405dc739152df54a241d6e5
dev0869 <108985813+dev0869@users.noreply.github.com>
2023-02-18 00:27:02 -0500

    as

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const PORT =5000;
 app.get('/',(req,res)=>{
     res.send('hii world')
 })
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use('/DeveshApi/Products/',Router)
 
 Connect().then(console.log('mongo is connected')).catch((error)=>{
